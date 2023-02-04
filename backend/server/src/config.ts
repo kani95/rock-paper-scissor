9e85dca49e0749f079126748614f34ab0e8a8e08
kasni325 <kasni325@student.liu.se>
2023-02-04 11:35:34 +0100

    fixed congig tests

--- backend/server/src/config.ts
+++ backend/server/src/config.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();    
 
+
 /* istanbul ignore next */
 const config = {
-    port: process.env.PORT  || 8000,
+    port: process.env.PORT || "" + 8000,
     endpoint: process.env.ENDPOINT || 'http://localhost:8000/api/games',
     prefix : process.env.PREFIX || '/api/games',
 }
