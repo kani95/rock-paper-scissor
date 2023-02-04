2b9dc68a605ef4fbf0ca12f627409e9c2a2b2583
kasni325 <kasni325@student.liu.se>
2023-02-04 11:40:28 +0100

    changed to strict object

--- backend/server/src/routes/schema/zodSchema.ts
+++ backend/server/src/routes/schema/zodSchema.ts
@@ -4,12 +4,12 @@ import Move from '../../../../db/model/Move';
 // schemas used by the routes to check the request body
 
 // schema for the player sent in the request body
-const playerSchema = zod.object({
+const playerSchema = zod.strictObject({
     name: zod.string().min(1).max(50)
 });
 
 // schema for the move sent in the request body
-const moveSchema = zod.object({
+const moveSchema = zod.strictObject({
     name: zod.string().min(1).max(50),
     move: zod.string().transform((value) => value.toLowerCase()).refine((value) => {
         return value === Move.ROCK || value === Move.PAPER || value === Move.SCISSORS;
@@ -17,7 +17,8 @@ const moveSchema = zod.object({
 });
 
 // schema for the id sent in the request body
-const idSchema = zod.object({
+// dont allow any more fields than id
+const idSchema = zod.strictObject({
     id: zod.string().min(1).max(70)
 });
 
